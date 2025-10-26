3b8f665318058d68ccb32cf2c83b21f309a3ee5c
Riya Rastogi <riya02rastogi@gmail.com>
2025-10-26 18:25:29 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
 import Welcome from "./pages/Welcome";
 import ChatUI from "./pages/ChatUI";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-      <Welcome />
-      <ChatUI />
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/chat" element={<ChatUI />} />
+      </Routes>
     </>
   );
 }
