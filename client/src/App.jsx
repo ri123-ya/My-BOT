9be1948656e2fc1644c52e867b1a6374cb49a360
Riya Rastogi <riya02rastogi@gmail.com>
2025-10-26 17:33:16 +0530

    initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Welcome from './pages/Welcome'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Welcome/>
+  )
+}
+
+export default App
