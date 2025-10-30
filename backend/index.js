855a99d2e56cbe2cc260ef42c6e3a49968fbee3c
Riya Rastogi <riya02rastogi@gmail.com>
2025-10-30 08:07:23 +0530

    added cors config

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,16 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
+app.use(cors({
+   origin: process.env.FRONTEND_URL,
+   credential: true
+}));
 
 app.use("/api", chatRoute);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 
 app.listen(process.env.PORT, () => {
