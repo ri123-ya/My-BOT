ec5eefa6e31e3b2e1f2616afb9b2c7759cb5ea58
Riya Rastogi <riya02rastogi@gmail.com>
2025-10-30 07:45:26 +0530

    added chatroute

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import chatRoute from "./routes/chatRoute.js";
 
 const app = express();
 dotenv.config();
 
+app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Hello World!");
+app.use("/api", chatRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(process.env.PORT,()=>{
-     console.log(`Server is running on port: ${process.env.PORT}`);
-})
+
+app.listen(process.env.PORT, () => {
+  console.log(`🚀 Server is running on port: ${process.env.PORT}`);
+});
