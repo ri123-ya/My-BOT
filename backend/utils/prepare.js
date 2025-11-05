0b8d02dbad657393c95ff50f00228c2bd2c5530f
Riya Rastogi <riya02rastogi@gmail.com>
2025-11-05 11:59:59 +0530

    updated chunking to use splitDocuments

--- backend/utils/prepare.js
+++ backend/utils/prepare.js
@@ -6,50 +6,34 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const filePath = "./data/Riya_Rastogi_SDE_25.pdf";
+const filePath = "./data/Resume.pdf";
 
 export async function indexTheDocument(filePath) {
   //Load the Pdf
   console.log("🔄 Loading PDF...");
   const loader = new PDFLoader(filePath, { splitPages: false });
   const doc = await loader.load();
-  const content = doc[0].pageContent;
   console.log("✅ PDF loaded");
 
-  // console.log("Document Loaded: ", doc[0].pageContent);
+  console.log("Document Loaded: ", doc[0].pageContent);
 
   //Chunking
-  // const textsplitters = new RecursiveCharacterTextSplitter({
-  //     chunkSize:700,
-  //     chunkOverlap:200,
-  // });
-  // const chunks = await textsplitters.splitText(doc[0].pageContent);
-  // console.log("Chunks : ", chunks);
+  const textsplitters = new RecursiveCharacterTextSplitter({
+    chunkSize: 700,
+    chunkOverlap: 200,
+    separators: ["\n\n", "\n", " ", ""]
+  });
+  const chunks = await textsplitters.splitDocuments(doc);
+  console.log("Chunks : ", chunks);
 
-  // Split by major sections
+  
   console.log("🔄 Chunking document...");
-  const sections = {
-    contact: extractSection(content, "Riya Rastogi", "Education"),
-    education: extractSection(content, "Education", "Projects"),
-    projects: extractSection(content, "Projects", "Technical Skills"),
-    skills: extractSection(content, "Technical Skills", "Competitions"),
-    achievements: extractSection(content, "Competitions", "Experience"),
-    experience: extractSection(content, "Experience", null),
-  };
-
-  // Create chunks with metadata
-  const chunks = Object.entries(sections).map(([section, text]) => ({
-   pageContent: text,
-    metadata: { section, source: "resume" },
-  }));
   console.log(`✅ Created ${chunks.length} chunks`);
-  //console.log("Semantic chunks:", chunks);
 
-
-  //Make Embeddings
-   console.log("🔄 Initializing embedding model...");
+  // Make Embeddings
+  console.log("🔄 Initializing embedding model...");
   const embeddingModel = new GoogleGenerativeAIEmbeddings({
-     modelName: "text-embedding-004",
+    modelName: "text-embedding-004",
   });
 
   //Store in Vector DB
@@ -64,15 +48,9 @@ export async function indexTheDocument(filePath) {
     }
   );
   console.log("✅ All vectors stored in Qdrant successfully!");
-  console.log(`📊 Collection: "company-bot" contains ${chunks.length} vectors`);
-  
+  console.log(`📊 Collection: "My-Bot" contains ${chunks.length} vectors`);
+
   return vectorStore;
 }
 
 indexTheDocument(filePath);
-
-function extractSection(text, startMarker, endMarker) {
-  const start = text.indexOf(startMarker);
-  const end = endMarker ? text.indexOf(endMarker) : text.length;
-  return text.substring(start, end).trim();
-}
